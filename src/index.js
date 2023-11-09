e7952b248dc267ccc9a2bf3ba49ebfb49283a610
abelcg <abelcg86@gmail.com>
2023-11-09 09:29:43 -0300

    add db connetion and middlewares

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+import './database'
+
+//console.log('desde mi backend');
+
+//crear la instancia de Express
+const app = express();
+
+//configurar el puerto
+
+app.set('port', process.env.PORT || 4002);
+
+app.listen(app.get('port'), () => {
+  console.log('====================================');
+  console.log('Estoy en el puerto ' + app.get('port'));
+  console.log('====================================');
+});
+
+//middlewares
+app.use(morgan('dev')); //nos da info de la consulta: tipo, status, el tiempo de ejecución.
+app.use(cors()); //nos permite recibir peticiones remotas a nuestra API
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //Estos dos últimos nos permiten recibir e interpretar el json de la req
+
+//app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+//rutas de prueba
+
+app.get('/', (req, res) => {
+  res.send('Esto es una prueba de mi backend');
+});
+
+app.delete('/borrarProducto', (req, res) => {
+  res.send('se borró el producto');
+});

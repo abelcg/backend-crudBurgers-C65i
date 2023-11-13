f9c4957eec56098e1662b9652983a918795fbca0
abelcg <abelcg86@gmail.com>
2023-11-13 09:40:07 -0300

    add crud logic

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -7,24 +7,80 @@ productController.showProducts = (req, res) => {
 export default productController;
  */
 
-const showProducts = (req, res) => {
-  res.send('Listar productos');
+import Product from '../models/product';
+
+const showProducts = async (req, res) => {
+  //res.send('Listar productos');
+  try {
+    //recuperar un arreglo con los productos de la BD
+    const productsList = await Product.find();
+    res.status(200).json(productsList);
+  } catch {
+    res.status(404).json({ message: 'Error searching for requested products' });
+  }
 };
 
-const getOne = (req, res) => {
-  res.send('El producto encontrado');
+const getOne = async (req, res) => {
+  //res.send('El producto encontrado');
+  try {
+    console.log(req.params.id);
+    const { id } = req.params;
+    const productFound = await Product.findById(id);
+    res.status(200).json(productFound);
+  } catch {
+    res.status(404).json({ message: 'Error searching for requested product' });
+  }
 };
 
-const createProduct = (req, res) => {
-  res.send('se creó el producto');
+const createProduct = async (req, res) => {
+  //res.send('se creó el producto');
+  const { productName, price, urlImg, category } = req.body;
+
+  try {
+    //console.log(req.body);
+    //validar
+
+    //crear un objeto para guardarlo en la BD
+    const newProduct = new Product({
+      /*  productName: req.body.productName,
+      price: req.body.price,
+      urlImg: req.body.urlImg,
+      category: req.body.category */
+      productName,
+      price,
+      urlImg,
+      category,
+    });
+
+    //guardar en la BD
+    await newProduct.save();
+    res.status(201).json({ message: 'Product created successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: 'Error creating product' + error });
+  }
 };
 
-const updateProduct = (req, res) => {
-  res.send('se actualizó el producto');
+const updateProduct = async (req, res) => {
+  //res.send('se actualizó el producto');
+  try {
+    //busque el producto por su id en la BD y lo actualice
+    await Product.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ message: 'Product successfully updated' });
+  } catch {
+    res.status(404).json({ message: 'Error searching for requested product' });
+  }
 };
 
-const deleteProduct = (req, res) => {
-  res.send('se borró el producto');
+const deleteProduct = async (req, res) => {
+  // res.send('se borró el producto');
+  try {
+    //busque el producto por su id en la BD y lo borre
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch {
+    res.status(404).json({ message: 'Error searching for requested product' });
+  }
 };
 
 export { showProducts, getOne, createProduct, deleteProduct, updateProduct };
--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://127.0.0.1:27017/crudburgers66i';
+const url = 'mongodb://127.0.0.1:27017/crudburgers65i';
 
 mongoose.connect(url);
 
